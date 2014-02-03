e4f8c4648602a264afdb44e285ab3a3b40d3cec2
Mathias Bogaert <mathias.bogaert@gmail.com>
2014-02-04 00:10:24 +0100

    WIP

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -4,7 +4,7 @@ import play.api.mvc._
 import play.modules.reactivemongo.MongoController
 
 import reactivemongo.api.collections.default.BSONCollection
-import reactivemongo.bson.BSONDocument
+import reactivemongo.bson.{BSONArray, BSONDocument}
 import models.{Location, Party}
 import play.api.libs.concurrent.Execution.Implicits._
 import scala.concurrent.Future
@@ -13,9 +13,11 @@ import scala.Some
 import reactivemongo.api.collections.default.BSONCollection
 import play.modules.reactivemongo.json.BSONFormats._
 import play.extras.geojson._
+import models.JsonFormats._
 
 object Application extends Controller with MongoController {
   def partyCollection: BSONCollection = db.collection[BSONCollection]("parties")
+
   def postcodeUnitCollection: BSONCollection = db.collection[BSONCollection]("pcu")
 
   implicit val locationWites = Json.writes[Location]
@@ -28,19 +30,26 @@ object Application extends Controller with MongoController {
   def indexWithMarker(partyId: String) = Action.async {
     val partyOption = partyCollection.find(BSONDocument("cid" -> partyId)).one[Party]
     partyOption.map {
-      case Some(party) =>
-        Ok(views.html.index(party.location, true, Some(party.name)))
+      case Some(party) => Ok(views.html.index(party.location, true, Some(party.name)))
       case None => NotFound("not found")
     }
   }
 
-  def search = Action.async(parse.json) { request =>
-    Console.println(request.body)
+  def search = Action.async(parse.json) {
+    request =>
+      Console.println(request.body)
 
-    /*val parties = partyCollection
-      .find(BSONDocument("loc" -> BSONDocument("$geoWithin" -> BSONDocument("$geometry" -> ))))
-      .cursor[Party]*/
+      val parties = partyCollection
+        .find(BSONDocument("loc" ->
+        BSONDocument("$geoWithin" -> BSONDocument("$geometry" -> BSONDocument("type" -> "Polygon", "coordinates" ->
+          BSONArray(BSONArray(BSONArray(-2.1368408203125,53.04946903057477), BSONArray(-0.9118652343749999,52.85420569896367), BSONArray(-0.96954345703125,53.131941982174006), BSONArray(-2.1368408203125,53.04946903057477))))))))
+        .cursor[Party]
 
-    Future.successful(Ok(Json.toJson(List(Party("2", "name", "email", "", true, Location(-2.40396726, 53.67323978))))))
+      val futurePartiesList: Future[List[Party]] = parties.collect[List]()
+
+      futurePartiesList.map {
+        persons =>
+          Ok(Json.toJson(persons))
+      }
   }
 }
